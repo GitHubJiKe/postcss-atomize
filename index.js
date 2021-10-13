4033a798767e2466814ee61911193c351fb5ca1d
yuanpengfei03 <yuanpengfei03@meituan.com>
2021-10-13 17:34:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = (opts = {}) => {
+
+  // Work with options here
+
+  console.log(opts);
+
+  return {
+    postcssPlugin: 'postcss-atomize',
+    /*
+    Root (root, postcss) {
+      // Transform CSS AST here
+    }
+    */
+
+    /*
+    Declaration (decl, postcss) {
+      // The faster way to find Declaration node
+    }
+    */
+
+    /*
+    Declaration: {
+      color: (decl, postcss) {
+        // The fastest way find Declaration node if you know property name
+      }
+    }
+    */
+  }
+}
+module.exports.postcss = true
